6971a3e30bf7053031e665405da8d835b40656d8
zarineo <tochilka007@mail.ru>
2024-03-08 09:00:13 +0300

    Строка поиска

--- js/search.js
+++ js/search.js
@@ -1,4 +1,7 @@
-const searchText = window.location.search;
+const searchText = window.location.search.substring(8).replaceAll('%20', ' ');
 
-console.log(searchText.substring(8).replace('%', ' '));
+console.log(searchText.substring(8).replaceAll('%20', ' '));
 
+fetch('https://jsonplaceholder.typicode.com/posts?title=${searchText}')
+.then(res => res.json()) //получаем ответ res и преобразовываем ему в json формат
+.then(res => console.log(res))
--- js/search_btn.js
+++ js/search_btn.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('.btn');
 const formControl = document.querySelector('.form-control');
 const getSearchForm = (e) => {
     e.preventDefault();
-    window.location.href = window.location.origin + `/post/search.html?search=${formControl.value}`;
+    window.location.href = window.location.origin + `/search.html?search=${formControl.value}`;
     console.log(window.location.origin.slice(5));
     formControl.value = '';
 }
